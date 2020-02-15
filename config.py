b1904b11b8194ad605351d90ffa40ebf193b7de3
oushMusa <oushmusa@gmail.com>
2020-02-15 23:04:24 +0700

    Blacked code

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     DEBUG = True
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you_will_never_guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "you_will_never_guess"
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL"
+    ) or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
